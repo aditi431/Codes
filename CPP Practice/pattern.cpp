fdf9b09cb623a66ca7726d05ae9d9e8bd30875af
aditi431 <aditisha15@gmail.com>
2026-03-13 22:46:01 +0530

    Add files via upload

---
+++ CPP Practice/pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+for(int j=1;j<=3;j++){
+    for(int i=1;i<=6;i++){
+        cout<<"*";
+        
+    }
+    
+    cout<<endl;
+}
+
+
+
+
+
+}
